a1e82770a62d2e639b700e2034c98c8987f3b936
heyheyhey <2025750483@qq.com>
2024-06-24 17:13:27 +0800

    Create 收集瓶盖赢大奖.cpp
    题目描述
    某饮料公司最近推出了一个"收集瓶盖赢大奖"的活动:如果你拥有10个印有"幸运"、或20个印有"鼓励"的瓶盖,就可
    以兑换一个神秘大奖。现分别给出你拥有的印有"幸运"和"鼓励"的瓶盖数,判断是否可以去兑换大奖。若可以兑换大
    奖,输出1,否则输出0。
    输入格式
    一行,包含两个整数,分别是印有"幸运"和"鼓励"的瓶盖数,用一个空格隔开。
    输出格式
    一行。若可以兑换大奖,输出1,否则输出0。

---
+++ 收集瓶盖赢大奖.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main(){
+	double x, y;
+	cin >> x >> y;
+	if (x>=10 || y>=20){
+		cout << 1;
+	}
+	else
+		cout << 0; 
+}
