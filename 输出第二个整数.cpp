3c8a60d0e8d70bde3b8e036b3a189560c35154b4
heyheyhey <2025750483@qq.com>
2024-06-24 17:14:03 +0800

    Create 输出第二个整数.cpp
    题目描述
    输入三个整数,整数之间由一个空格分隔。把第二个输入的整整数输出
    输入格式
    只有一行,共三个整数,整数之间由一个空格分隔。
    输出格式
    只有一行,一个整数,即输入的第二个整数。

---
+++ 输出第二个整数.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main(){
+	 double a[3];
+	 for(int i=0; i<3; i++){
+	 	cin >> a[i];
+	 }
+	 cout << a[1];
+}
