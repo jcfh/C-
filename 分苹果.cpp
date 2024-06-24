9a61bad8d7d7d3ac4463d170e1113214dfb16442
heyheyhey <2025750483@qq.com>
2024-06-24 18:37:33 +0800

    Create 分苹果.cpp
    题目描述
    把一堆苹果分给n个小朋友,要使每个人都能拿到苹果,而且每个人拿到的苹果数都不同
    的话,这堆苹果至少应该有多少个?
    输入格式
    一个不大于1000的正整数n,代表小朋友人数。
    输出格式
    一个整数,表示满足条件的最少苹果个数。

---
+++ 分苹果.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main(){
+	 double n, sum;
+	 cin >> n;
+	 sum = (1+n)/2*n;
+	 cout << sum;
+}
