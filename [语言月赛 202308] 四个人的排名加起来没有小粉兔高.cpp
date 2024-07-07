64662a5a6fcea0fbc9ad5efec47fccd588d88174
heyheyhey <2025750483@qq.com>
2024-07-07 16:20:52 +0800

    Create [语言月赛 202308] 四个人的排名加起来没有小粉兔高.cpp
    题目描述
    又是一年NOI结束了。小A,小B,小C和小D作为正式选手,在NOI3202排名分
    别是第a名,第b名,第c名和第d名。
    现在,小A,小B,小C和小D都被保送到清华大学,并住进同一间寝室。
    如果四个人的NOI排名之和小于51,则我们称『四个人的排名力加起来没有小粉兔
    高』;反之,如果四个人的NOI排名之和大于或等于51,我们称『四个人的排名加
    起来不比小粉兔低』。
    现在,扶苏知道了四个人的NOI排名,她想请你判断是否『四个人的排名加起来没有
    小粉兔高』。
    输入格式
    输入只有一行四个整数,依次表示四个人的NOI排名 a, b,c,d。
    输出格式
    输出一行一个字符串表示答案:
    如果『四个人的排名加起来没有小粉兔高』,输出Rabbitwins,
    如果『四个人的排名加起来不比小粉兔低』,输出Rabbitlose.

---
+++ [语言月赛 202308] 四个人的排名加起来没有小粉兔高.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	if (a+b+c+d<51) {
+		cout << "Rabbit wins";
+	}
+	else
+		cout << "Rabbit lose";
+}
